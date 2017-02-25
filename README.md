720ce2a35190ddc93655417aa1faf9477e8a4ee8
Ivan Bogachev <sfi0zy@gmail.com>
2017-02-25 12:04:15 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Ucavatar.js
+Ucavatar is a little standalone library for generating unique avatars.
+
+![Preview of Ucavatar](https://raw.githubusercontent.com/sfi0zy/ucavatar/master/preview.png)
+
+## Installation
+From NPM
+```sh
+npm insall --save ucavatar
+```
+```js
+var Ucavatar = require('ucavatar');
+```
+
+Or You can copy it into your project and use a script tag
+```html
+<script src='ucavatar.js'></script>
+```
+
+## Examples
+At first, You need a canvas:
+```html
+<canvas id='avatar'></canvas>
+```
+You can generate an avatar with the following code:
+```js
+Ucavatar('#avatar', 'my name, nickname or email')
+```
+By default size of an avatar is 64x64, but You can specify a custom one:
+```js
+Ucavatar('#avatar', 'my name', 128)
+```
+
+## Enjoy it!
+Ucavatar is based on my pen [Unique Avatars Generator (prototype)](http://codepen.io/sfi0zy/pen/kkBAjd), it is a beta version and I have not very much time to work on it. If you have an idea how to improve Ucavatar - You can fork it, make changes and create a pull request, or contact me by email (sfi0zy@gmail.com).
+
---
+++ ucavatar.js
@@ -0,0 +1,82 @@
+// Ucavatar.js
+// Unique avatars generator
+//
+// Author: Ivan Bogachev <sfi0zy@gmail.com>, 2017
+// License: MIT
+
+
+function strangeHash(str) {
+    var hash = 0;
+    
+    if (str.length === 0) {
+        return hash;
+    }
+    
+    for (var i = 0, len = str.length; i < len; i++) {
+        var chr = str.charCodeAt(i);
+
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0;
+    }
+    
+    return (new Array(9).join('1') + Math.abs(hash)).slice(-8).replace(/0/g, '1');
+};
+
+
+function Ucavatar(canvas, str, size) {
+    if (typeof canvas === 'string') {
+        canvas = document.querySelector(canvas);
+    }
+
+    if (!canvas || !document.body.contains(canvas)) {
+        throw new Error('Ucavatar: canvas does not exists');
+    }
+
+    if (!size) {
+        size = 64;
+    }
+
+    canvas.height = size;
+    canvas.width = size;
+
+    var ctx  = canvas.getContext('2d'),
+        h    = strangeHash(str);
+    
+    ctx.translate(size/2, size/2);
+    ctx.rotate(Math.PI / 4);
+    
+    for (var i = 0; i < 8; i++) {
+        var r = h[0] * 100 % 255,
+            g = h[i] * 100 % 255,
+            b = h[8 - i] * 100 % 255,
+            a = h[i] / 20;
+       
+        ctx.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+        
+        var c1 = h[i]*h[i] % (size/4),
+            c2 = h[i]*h[7] % (size/4),
+            c3 = h[i]*h[0] % (size/2),
+            c4 = h[i]*h[0] % (size/2);
+        
+        if (c1 + c2 < (size/2)) {
+            c1 *= -2;
+            c2 *= -2;
+            c3 *=  2;
+            c4 *=  2;
+        }
+        
+        var s = h[7] % 2 + 2;
+        
+        for (var j = 0; j < 2 * s; j++) {
+            ctx.fillRect(c1, c2, c3, c4);
+            ctx.rotate(Math.PI / s);
+        }
+    }
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports.Ucavatar = Ucavatar;
+} else {
+    window.Ucavatar = Ucavatar;
+}
+
